f2b5fa0d49d92f63a2a48d3f948b2caf0c1ba6f1
Saeid Akbari <saeidscorp@yahoo.com>
2016-03-31 00:51:22 +0430

    changed routes to point to proper templates. (which isn't now functional, because of missing context vars).

--- src/clj/site/routes/blog.clj
+++ src/clj/site/routes/blog.clj
@@ -7,16 +7,16 @@
 (defn latest-posts [title])
 
 (defroutes blog-routes
-   (GET "/blog/latest-posts" [] (layout/render "blog/multi-card-side.html" {}))
-   (GET "/blog/card-boxed" [] (layout/render "blog/card-boxed.html"))
-   (GET "/blog/card-side" [] (layout/render "blog/card-side.html"))
-   (GET "/blog/multi-full" [] (layout/render "blog/multi-full.html"))
-   (GET "/blog/single-full" [] (layout/render "blog/single-full.html"))
-   (GET "/blog/single-side" [] (layout/render "blog/single-side.html"
-                                  {:breadcrumb-path
-                                                  [{:href "/" :name "Home"}
-                                                   {:href "/blog" :name "blog"}]
-                                    :popular-tags [{:href "/tag/clojure" :name "Clojure"}
-                                                   {:href "/tag/java" :name "Java"}]
-                                    :categories   [{:href "/tag/clojure" :name "Clojure"}
-                                                   {:href "/tag/java" :name "Java"}]})))
+           (GET "/blog/multi-card-boxed" [] (layout/render "blog/multi-card-boxed.html")) ;; check
+           (GET "/blog/multi-card-side" [] (layout/render "blog/multi-card-side.html")) ;; check
+           (GET "/blog/multi-full" [] (layout/render "blog/multi-full.html"))
+           (GET "/blog/multi-side" [] (layout/render "blog/multi-side.html"))
+           (GET "/blog/single-full" [] (layout/render "blog/single-full.html")) ;; check
+           (GET "/blog/single-side" [] (layout/render "blog/single-side.html" ;; check
+                                                      {:breadcrumb-path
+                                                                     [{:href "/" :name "Home"}
+                                                                      {:href "/blog" :name "blog"}]
+                                                       :popular-tags [{:href "/tag/clojure" :name "Clojure"}
+                                                                      {:href "/tag/java" :name "Java"}]
+                                                       :categories   [{:href "/tag/clojure" :name "Clojure"}
+                                                                      {:href "/tag/java" :name "Java"}]})))
